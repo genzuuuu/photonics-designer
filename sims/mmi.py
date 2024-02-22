965d21b9ac6afe0bfdc40fe3a6dda3490172d930
JasonLiu321 <z749liu@uwaterloo.ca>
2024-02-22 14:24:47 -0500

    added splitting ratio and insertion loss calculation methods

--- sims/mmi.py
+++ sims/mmi.py
@@ -9,6 +9,9 @@ from gdsfactory.generic_tech import LAYER_STACK, get_generic_pdk
 
 import gplugins.lumerical as sim
 
+import re
+import math
+
 #remoteArgs = { "hostname": license.hostname,"port": license.port }
 
 class mmi1x2:
@@ -41,3 +44,55 @@ class mmi1x2:
         with open('sim2.pk1','wb') as file: pickle.dump(a,file)
 
         #check if design is ok
+    
+
+    def process_line(line):
+    # Check if line starts with special characters
+    if re.match(r'^[\[\(]', line):
+        return None
+    else:
+        # Split the line into numbers
+        numbers = [float(num) for num in line.split()]
+        return numbers
+
+    def splitting_ratio_insertion_loss(filename, num_simulation):
+        # filename: file path to the .dat file 
+        # num_simulation: number of simulations
+        # return: insertion_loss, splitting_ratio
+        data = []
+        with open(filename, 'r') as file:
+            for line in file:
+                line = line.strip()
+                processed_line = process_line(line)
+                if processed_line is not None:
+                    data.append(processed_line)
+        
+        # Split the original array into chunks of 6 arrays each
+        chunks = [data[i:i + num_simulation] for i in range(0, len(data), num_simulation)]
+
+        # Assign each chunk to a new variable
+        for i, chunk in enumerate(chunks, 1):
+            globals()[f"result_{i}"] = chunk
+
+        # Print the newly created variables
+        for i in range(1, 10):
+            print(f"result_{i}: {globals()[f'result_{i}']}") # S11, S12 ... S33
+
+        T2 = []
+        T3 = []
+        insertion_loss = []
+        splitting_ratio = []
+
+        for x in range(6):
+        T2_temp = result_4[x][1]
+        T3_temp = result_7[x][1]
+        T2.append(T2_temp)
+        T3.append(T3_temp)
+        insertion_loss.append([result_4[x][0],10*math.log10(T3_temp+T2_temp)])
+        splitting_ratio.append([result_4[x][0],-10*math.log10(max(T2_temp,T3_temp)/min(T2_temp,T3_temp))])
+
+        data_1 = {"insertion loss":[], "splitting ratio":[]}
+        data_1.update({"insertion loss": insertion_loss, "splitting ratio": splitting_ratio})
+
+        # output: {'insertion loss': [[wavelength,...], [wavelength,...]...], 'splitting ratio':[[wavelength,...], [wavelength,...]...]}
+        return data_1

4e90b0113f4fa5359270cea6f676eed83dbeb18f
cheickdo <93621548+cheickdo@users.noreply.github.com>
2024-03-01 12:31:21 +0100

    MMI specifications are now a variable

--- sims/mmi.py
+++ sims/mmi.py
@@ -2,23 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-#import lumapi
-
-
-
 #import lumapi on MPI computers
 import sys, os
 sys.path.append("C:\\Program Files\\Lumerical\\v211\\api\\python\\") 
 sys.path.append(os.path.dirname(__file__)) #Current directory
-
-#import importlib.util
-#spec_win = importlib.util.spec_from_file_location('lumapi', 'C:\\Program Files\\Lumerical\\v211\\api\\python\\lumapi.py')
-#lumapi = importlib.util.module_from_spec(spec_win) #windows
-#spec_win.loader.exec_module(lumapi)
 import lumapi
 
 import gdsfactory as gf
 from gdsfactory.generic_tech import LAYER_STACK, get_generic_pdk
+from gplugins.common.utils.get_sparameters_path import (
+    get_sparameters_path_lumerical as get_sparameters_path,
+)
+from gdsfactory.pdk import get_layer_stack
 
 import gplugins.lumerical as sim
 import sqlite3
@@ -26,13 +21,9 @@ import re
 import math
 import spicy
 
-
 class mmi1x2:
 
-
-    component = None
-
-    def __init__(self,**kwargs):
+    def __init__(self, Width_MMI = 3.8, Length_MMI=12.8, mmiid=None, Gap_MMI=0.25, Taper_Length=10, Taper_Width=1.4,**kwargs):
         
         #defaults    
         self.parameters = dict(
@@ -47,25 +38,27 @@ class mmi1x2:
             ymargin = 1,
             zmargin = 1,
             overwrite=True,
+            dirpath = None,
         ) 
 
         self.component = None
+        self.layer_stack=get_layer_stack()
         self.num_port = 3
         self.sparam = None
         self.IL_SR = None
 
-        self.mmiid = None
-        self.Width_MMI = 3.8 
-        self.Length_MMI = 12.8  
-        self.Gap_MMI = 0.25 
-        self.Taper_Length = 10 
-        self.Taper_Width = 1.4  
+        self.mmiid = mmiid
+        self.Width_MMI = Width_MMI
+        self.Length_MMI = Length_MMI
+        self.Gap_MMI = Gap_MMI
+        self.Taper_Length = Taper_Length
+        self.Taper_Width = Taper_Width
 
         self.center_wavelength = None
         self.start_bandwidth = None
         self.stop_bandwidth = None
         self.mean_IL = None
-        self.mean_sr = None
+        self.mean_SR = None
         self.IL_center = None
         self.SR_center = None
         self.file_path = None
@@ -81,6 +74,7 @@ class mmi1x2:
         self.parameters.update(kwargs)
         self.__dict__.update(self.parameters)
         print(self.parameters)
+        
         #TODO
         #search database for design 
     
@@ -91,22 +85,19 @@ class mmi1x2:
         PDK.activate()
 
         #change such that it takes in some of the parameter values
-        self.component = gf.components.cells["mmi1x2"]()
+        self.component = gf.components.cells["mmi1x2"](width_mmi=self.Width_MMI, length_mmi=self.Length_MMI, gap_mmi=self.Gap_MMI,
+                                                       length_taper=self.Taper_Length, width_taper=self.Taper_Width)
 
-    def run(self, fname, dirpath=None):
+    def run(self):
         #ok checks
         #simulate gds to get s_parameters
         s = lumapi.FDTD(hide=True)
 
-        a = sim.write_sparameters_lumerical(self.component, run=True, session=s, dirpath=dirpath,  **self.parameters)
-        
-        #check specs related to s_parameters
-        
-        with open(fname,'wb') as file: pickle.dump(a,file)
+        a = sim.write_sparameters_lumerical(self.component, run=True, session=s,  **self.parameters)
         
         # get S parameters from the simulation
         self.sparam = s.getsweepresult("s-parameter sweep", "S parameters")
-        #print(self.sparam)
+        
         #check if design is ok
 
 
@@ -127,17 +118,22 @@ class mmi1x2:
             data_1.update({"insertion loss": insertion_loss, "splitting ratio": splitting_ratio})
 
             # output: {'insertion loss': [[wavelength,...], [wavelength,...]...], 'splitting ratio':[[wavelength,...], [wavelength,...]...]}
-        print(data_1)
         self.IL_SR = data_1
         
-    def insert_into_database(self, file_path, database_path): 
+    def insert_into_database(self, database_path): 
+        #file_path = get_sparameters_path(
+        #    component=self.component,
+        #    layer_stack=self.layer_stack,
+        #    **self.parameters,
+        #)
+        file_path= None
         num_simulation = self.parameters['wavelength_points']
         conn = sqlite3.connect(database_path)
         print("[INFO] : Successful connection!")
 
         # reads the biggest number of MMIID 
         cur = conn.cursor()
-        sql_sel_max_query = '''SELECT MAX(MMIID) FROM MMI'''
+        sql_sel_max_query = '''SELECT MAX(MMIID) FROM MMI1x2'''
         cur.execute(sql_sel_max_query)
         MMIID = cur.fetchall()[0][0]
         if (MMIID == None): 
@@ -169,7 +165,7 @@ class mmi1x2:
         data_subset_SR = []
         if (self.start_bandwidth is None or self.stop_bandwidth is None):
             self.mean_IL = None
-            self.mean_sr = None
+            self.mean_SR = None
         else: 
             for i in range(num_simulation):
                 if (self.IL_SR['insertion loss'][i][0] <= self.stop_bandwidth and self.IL_SR['insertion loss'][i][0] >= self.start_bandwidth ):
@@ -180,10 +176,11 @@ class mmi1x2:
 
         self.file_path = file_path
         # insert .dat file path along with MMI specs into MMI table into a new row
-        sql_insert_data_query = '''INSERT INTO MMI(MMIID, WidthMMI, LengthMMI, GapMMI, LengthTaper, WidthTaper, CenterWavelength, StartBandwidth, StopBandwidth, MeanIL, MeanSR, ILCenter, SRCenter,  FilePath)
+        sql_insert_data_query = '''INSERT INTO MMI1x2(MMIID, WidthMMI, LengthMMI, GapMMI, LengthTaper, WidthTaper, CenterWavelength, StartBandwidth, StopBandwidth, MeanIL, MeanSR, ILCenter, SRCenter,  FilePath)
         VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?);'''
         cur = conn.cursor()
-        cur.execute(sql_insert_data_query, (MMIID, self.Width_MMI, self.Length_MMI, self.Gap_MMI, self.Taper_Length, self.Taper_Width,self.center_wavelength, self.start_bandwidth, self.stop_bandwidth, self.mean_IL, self.mean_SR, self.IL_center, self.SR_center,  file_path))
+        cur.execute(sql_insert_data_query, (MMIID, self.Width_MMI, self.Length_MMI, self.Gap_MMI, self.Taper_Length, self.Taper_Width,self.center_wavelength, self.start_bandwidth, self.stop_bandwidth, self.mean_IL, self.mean_SR, self.IL_center, self.SR_center,  self.
+                                            file_path))
         conn.commit()
         print("[INFO] : ", file_path, "is in the database.") 
         print("[INFO] : This is entry number:", self.mmiid) 
@@ -195,7 +192,7 @@ class mmi1x2:
 
         # reads the biggest number of MMIID 
         cur = conn.cursor()
-        sql_insert_file_query = ''' SELECT * FROM (SELECT * FROM MMI WHERE %s>StartBandwidth and %s<StopBandwidth  ORDER BY ABS(CenterWavelength - %s) LIMIT 3) AS subquery_table  ORDER BY ILCenter+SRcenter ASC LIMIT 1 ; '''
+        sql_insert_file_query = ''' SELECT * FROM (SELECT * FROM MMI1x2 WHERE %s>StartBandwidth and %s<StopBandwidth  ORDER BY ABS(CenterWavelength - %s) LIMIT 3) AS subquery_table  ORDER BY ILCenter+SRcenter ASC LIMIT 1 ; '''
         cur.execute(sql_insert_file_query, (center_wavelength))
         row = cur.fetchall()
         print("[INFO] : Successful Query!")
@@ -207,10 +204,17 @@ class mmi1x2:
         print("[INFO] : Successful connection!")
         # reads the biggest number of MMIID 
         cur = conn.cursor()
-        sql_edit_query = '''UPDATE MMI SET WidthMMI= %s, LengthMMI= %s, GapMMI= %s, LengthTaper= %s, WidthTaper= %s, CenterWavelength= %s, StartBandwidth= %s, StopBandwidth= %s, MeanIL= %s, MeanSR= %s, ILCenter= %s, SRCenter= %s,  FilePath= %s WHERE ID = %s; '''
+        sql_edit_query = '''UPDATE MMI1x2 SET WidthMMI= %s, LengthMMI= %s, GapMMI= %s, LengthTaper= %s, WidthTaper= %s, CenterWavelength= %s, StartBandwidth= %s, StopBandwidth= %s, MeanIL= %s, MeanSR= %s, ILCenter= %s, SRCenter= %s,  FilePath= %s WHERE ID = %s; '''
         cur.execute(sql_edit_query, (self.Width_MMI, self.Length_MMI, self.Gap_MMI, self.Taper_Length, self.Taper_Width,self.center_wavelength, self.start_bandwidth, self.stop_bandwidth, self.mean_IL, self.mean_SR, self.IL_center, self.SR_center,  self.file_path, self.mmiid))
         conn.commit()
         print("[INFO] : Entry modified")
+        
+    #runs full simulation and inserts into database
+    def runall(self, database_path):
+        self.draw_gds()
+        self.run()
+        self.splitting_ratio_insertion_loss()
+        self.insert_into_database(database_path=database_path)
 
 
 def fitness_function(input_param):
@@ -250,19 +254,46 @@ def fitness_function(input_param):
 
     return mean_IL
 
+
 ##
 if __name__ == '__main__':
     #running of an example
+    #filepath = "C:\\Users\\cdoumbia\\Documents\\photonicsdesigner\\sims\\data\\mmi1x2\\mmi1x2_533af979379917b86f7c4ae7ba55821f.dat"
+    databasepath = "./MMIDB.db"
+    dir_path = "C:\\Users\\cdoumbia\\Documents\\photonicsdesigner\\sims\\data"
     
-
-    for mesh in range(12):
-        marg = 6
-        
-        c = mmi1x2(xmargin=marg, ymargin=marg, zmargin=marg, wavelength_points=5, mesh_accuracy=mesh+1, overwrite=False)#, Length_MMI = 12.8 , Gap_MMI = 0.25)
-        c.draw_gds()
-        c.run(fname=f"mesh{mesh+1}margin{marg}.pk1")
-        c.splitting_ratio_insertion_loss()
-
+    c = mmi1x2(xmargin=1, ymargin=1, zmargin=1, 
+               wavelength_points=5, mesh_accuracy=2, 
+               overwrite=True, dirpath=dir_path, Width_MMI=3, 
+               Length_MMI=10, Gap_MMI=0.2, Taper_Length=8, 
+               Taper_Width=9)
+    
+    c.runall(database_path=databasepath)
+    
+    c = mmi1x2(xmargin=2, ymargin=2, zmargin=2, 
+               wavelength_points=10, mesh_accuracy=5, 
+               overwrite=True, dirpath=dir_path, Width_MMI=5, 
+               Length_MMI=15, Gap_MMI=0.5, Taper_Length=12, 
+               Taper_Width=9)
+    
+    c.runall(database_path=databasepath)
+    
+    c = mmi1x2(xmargin=2, ymargin=2, zmargin=2, 
+               wavelength_points=5, mesh_accuracy=4, 
+               overwrite=True, dirpath=dir_path, Width_MMI=4, 
+               Length_MMI=11, Gap_MMI=0.3, Taper_Length=9, 
+               Taper_Width=7)
+    
+    c.runall(database_path=databasepath)
+    
+    c = mmi1x2(xmargin=3, ymargin=3, zmargin=3, 
+               wavelength_points=5, mesh_accuracy=2, 
+               overwrite=True, dirpath=dir_path, Width_MMI=3, 
+               Length_MMI=13, Gap_MMI=0.2, Taper_Length=8, 
+               Taper_Width=10)
+    
+    c.runall(database_path=databasepath)
+    
     # trying optimization function
     #res = spicy.optimize.minimize(fitness_function, (5.5,0.25,100),method='COBYLA')
     #print(res)

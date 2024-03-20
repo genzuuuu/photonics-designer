54c6c6b44f1a92ff88f34dd4de20bad5820eea51
JasonLiu321 <z749liu@uwaterloo.ca>
2024-03-20 12:09:31 -0400

    updated updateClass()

--- sims/mmi.py
+++ sims/mmi.py
@@ -175,8 +175,15 @@ class mmi1x2:
                 if (self.IL_SR['insertion loss'][i][0] <= self.stop_bandwidth and self.IL_SR['insertion loss'][i][0] >= self.start_bandwidth ):
                     data_subset_IL.append(self.IL_SR['insertion loss'][i][1])
                     data_subset_SR.append(self.IL_SR['splitting ratio'][i][1])
-            self.mean_IL = sum(data_subset_IL)/len(data_subset_IL)
-            self.mean_SR = sum(data_subset_SR)/len(data_subset_SR)    
+            if (data_subset_IL is None):
+                self.mean_IL = None
+            else:
+                self.mean_IL = sum(data_subset_IL)/len(data_subset_IL)
+            
+            if (data_subset_SR is None):
+                self.mean_SR = None
+            else:
+                self.mean_SR = sum(data_subset_SR)/len(data_subset_SR)    
 
         
     def insert_into_database(self): 
